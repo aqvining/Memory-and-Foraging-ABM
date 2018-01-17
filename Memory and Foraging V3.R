941c3cfb53091c028ce292e0cc5fc8a3ffa9551c
Alexander Vining <aqvining@ucdavis.edu>
2018-01-17 14:35:47 -0800

    Project Description
    Initial staging of R file for the model, contains only description

---
+++ Memory and Foraging V3.R
@@ -0,0 +1,8 @@
+###~~~Memory and Foraging Agent Based Model Version 3~~~###
+#The goal of this agent based model is to allow flexible and efficient exploration of the impact of spatial memory an animal foraging.
+#It will allow for manipulation of memory size, accuracy, and properties such as learning or forgetting rules.
+#The model will incorporate aspects of optiomal foraging theory into the animal behavior, and additional allow for different
+#navigational decision strategies. The model will then measure overall foraging efficiency given the different memory and
+#navigation properties.
+
+##Version 3.01 Notes:
