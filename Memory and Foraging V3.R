8e668e7f688283d7ed9b50e59a2d75a07c036981
Cyrus Parvereshi <cjparvereshi@ucdavis.edu>
2018-01-17 14:41:41 -0800

    Forager class object
    Created forager class and 2 subclasses to create constructors for

--- Memory and Foraging V3.R
+++ Memory and Foraging V3.R
@@ -6,3 +6,23 @@
 #navigation properties.
 
 ##Version 3.01 Notes:
+#allowing agent to hav a diff memory of environment resources than actually is there
+
+
+setClass("Forager", 
+         slots= list(
+           location = "numeric", # named vector of x and y
+           memory = "numeric", #data frame with columns: patch name, x location, y location, resource value at departure, 
+           #time since last visit, expected value
+           optForageThresh = "numeric") #slot for optimal foraging threshold, where the animal will leave if it stops gaining
+         # a certain amount of energy and will leave the current resource
+) 
+#define dem subclasses
+setClass("Agent1", contains = "Forager")
+setClass("Agent2", contains = "Forager")
+
+#constructor of agent 1
+agent1 <- function
+
+#bunny <- new('Forager', location = , memory = c(runif(1,rangeMin, rangeMax),runif(1,rangeMin, rangeMax)),
+#	         optForageThresh = runif(1,0,8))
