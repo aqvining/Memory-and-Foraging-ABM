c46132a40507a745335eaa2e7b8c95f69cdd63bc
Alexander Vining <aqvining@ucdavis.edu>
2018-01-17 17:13:02 -0800

    Alexander Edits
    Renamed forager class to foragerA to allow for more types in the future
    simplified setClass for foragerA
    Added comments for additional edits

--- Memory and Foraging V3.R
+++ Memory and Foraging V3.R
@@ -1,23 +1,13 @@
-#allowing agent to hav a diff memory of environment resources than actually is there
+setClass("foragerA", slots = list(location = "numeric", memory = "data.frame", optForageThresh = "numeric")) #simplified
 
-
-setClass("Forager", 
-	     slots= list(
-	     	location = "numeric", # named vector of x and y
-	     	memory = "numeric", #data frame with columns: patch name, x location, y location, resource value at departure, 
-	     	                    #time since last visit, expected value
-	     	optForageThresh = "numeric") #slot for optimal foraging threshold, where the animal will leave if it stops gaining
-	     									# a certain amount of energy and will leave the current resource
-) 
-#constructor 
-forager<- function(location = c(runif(1,0, 10),runif(1,0, 10)), 
+ForagerA<- function(location = c(runif(1,0, 10),runif(1,0, 10)), #instead of set values (0,10) can you have this take input xlim and ylim, then draw from -xlim to xlim (and same for y)
 	               memory = data.frame(patchName = "grass", xLocation = runif(1,0,10), yLocation = runif(1,0,10), 
 	               	                   resourceValAtDeparture = 3, timeSinceLastVisit  = 46, expectedValue = 87
-	               	                   ),
-	               optForageThresh = runif(1,0,20)
+	               	                   ), #instead of assigning values to this dataframe, please make it an empty struction
+	               optForageThresh = 0 #I changed this to 0 (a naive forager should eat any food available, not knowing if it will find any more)
 	               ) 
 {
-                   if(!length(location) == 2) stop ("location must be length 2")
+                   if(!length(location) == 2) stop ("location must be length 2") #good checks, thanks
                    if(!ncol(memory) == 6) stop ("memory should be a data frame with 6 columns")
                    newForager <- new("Forager", location = location, memory = memory, optForageThresh = optForageThresh)
                    names(newForager@location) <- c('x', 'y')
