b8da68fefac1a70a014ff6fc1c1adbe8252ac2f8
Alexander Vining <aqvining@ucdavis.edu>
2018-01-18 09:34:20 -0800

    Formatting and Main Function
    Formatted code into sections
    Added definition of runSimulation, the main function executable. Still needs to be completed

--- Memory and Foraging V3.R
+++ Memory and Foraging V3.R
@@ -6,30 +6,50 @@
 #navigation properties.
 
 ##Version 3.01 Notes:
-setGeneric("growth", function(x) standardGeneric("growth"))                 #creates generic function "growth" from which methods for patch resource growth can be built
-setGeneric("extraction", function(x) standardGeneric("extraction"))         #creates generic function "extraction" from which methods for patch resource growth can be built
+
+#~~ 1. Setup ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#~~~~ 1a. Class creation and constructor functions
 setClass("patchA", slots = list(location = 'numeric', energy = 'numeric'))  #sets class structure for patch type A
 
-setMethod(f = "growth", signature = "patchA", definition = function(x) {    #growth method for patch type A
-  ###input: object of class "patchA"
-  ###output: input object with energy slot increased by 1
-  x@energy <- x@energy + 1
-  x})
-
-setMethod(f = "extraction", signature = "patchA", definition = function(x) {    #extraction method for patch type A
-  ###input: object of class "patchA"
-  ###output: input object with energy slot decreased by 20%
-  x@energy <- 4 * x@energy/5
-  x})
-
-PatchA <- function(xlim, ylim) {                                                           #constructor for patchA class
+PatchA <- function(xlim, ylim) {                                  #constructor for patchA class
   ###input: "xlim" = maximum value for x coordinate of patch
   ###       "ylim" = maximum value for y coordinate of patch
   ###output: object of class patchA
   ###description: The location slot of output object will have x,y coordinates drawn from a uniform distribution between -xlim and xlim, and -ylim and ylim
   ###             The energy slot of output will have value of 5
   newPatchA <- new("patchA", location = c(runif(1, -xlim, xlim), runif(1, -ylim, ylim)), 
-                    energy = 5)
+                   energy = 5)
   names(newPatchA@location) <- c('x', 'y')
   newPatchA
 }
+
+#~~~~ 1b. Methods
+setGeneric("growth", function(x) standardGeneric("growth"))                 #creates generic function "growth" from which methods for patch resource growth can be built
+setMethod(f = "growth", signature = "patchA", definition = function(x) {    #growth method for patch type A
+  ###input: object of class "patchA"
+  ###output: input object with energy slot increased by 1
+  x@energy <- x@energy + 1
+  x})
+
+setGeneric("extraction", function(x) standardGeneric("extraction"))         #creates generic function "extraction" from which methods for patch resource growth can be built
+setMethod(f = "extraction", signature = "patchA", definition = function(x) {    #extraction method for patch type A
+  ###input: object of class "patchA"
+  ###output: input object with energy slot decreased by 20%
+  x@energy <- 4 * x@energy/5
+  x})
+
+#~~~~ 1c. Calculation Tools
+
+#~~ 2. Main Function ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+runSimulation <- function(numForager, numPatch, t, xSize, ySize){
+  ###input: numForager should be a named vector where each name is a forager class and each value is the number of that forager class to be created
+  ###       numPatch should be named vector where each name is a patch class and each value is the number of that patch class to be created
+  ###       t = time steps to run simulation
+  ###       xlim and ylim are the field size of the x and y coordinates respectively
+  ###output: A list of length two. The first element contains a list of foragers for each time step, the second element a list of patches
+  ###description: Architecture of simulation. Applies methods to all foragers and patches over a series of time steps
+  
+}
+
+
