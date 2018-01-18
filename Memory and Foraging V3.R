a426f940b5c497efdfad57850c6042640fcf5ef8
Alexander Vining <aqvining@ucdavis.edu>
2018-01-17 16:56:05 -0800

    Patch constructor and methods added
    Placeholder patchA class has growth method that adds 1 to energy and extraction method that removes 1/5 energy
    constructor function allows input for coordinate range (uniform draw) and assigns value of 5 to patch energy slot

--- Memory and Foraging V3.R
+++ Memory and Foraging V3.R
@@ -6,3 +6,30 @@
 #navigation properties.
 
 ##Version 3.01 Notes:
+setGeneric("growth", function(x) standardGeneric("growth"))                 #creates generic function "growth" from which methods for patch resource growth can be built
+setGeneric("extraction", function(x) standardGeneric("extraction"))         #creates generic function "extraction" from which methods for patch resource growth can be built
+setClass("patchA", slots = list(location = 'numeric', energy = 'numeric'))  #sets class structure for patch type A
+
+setMethod(f = "growth", signature = "patchA", definition = function(x) {    #growth method for patch type A
+  ###input: object of class "patchA"
+  ###output: input object with energy slot increased by 1
+  x@energy <- x@energy + 1
+  x})
+
+setMethod(f = "extraction", signature = "patchA", definition = function(x) {    #extraction method for patch type A
+  ###input: object of class "patchA"
+  ###output: input object with energy slot decreased by 20%
+  x@energy <- 4 * x@energy/5
+  x})
+
+PatchA <- function(xlim, ylim) {                                                           #constructor for patchA class
+  ###input: "xlim" = maximum value for x coordinate of patch
+  ###       "ylim" = maximum value for y coordinate of patch
+  ###output: object of class patchA
+  ###description: The location slot of output object will have x,y coordinates drawn from a uniform distribution between -xlim and xlim, and -ylim and ylim
+  ###             The energy slot of output will have value of 5
+  newPatchA <- new("patchA", location = c(runif(1, -xlim, xlim), runif(1, -ylim, ylim)), 
+                    energy = 5)
+  names(newPatchA@location) <- c('x', 'y')
+  newPatchA
+}
