cd2181f74c6709c82f57d33938a2252d61efdd64
Alexander Vining <aqvining@ucdavis.edu>
2018-01-24 17:04:26 -0800

    Merge Fixes

--- Memory and Foraging V3.R
+++ Memory and Foraging V3.R
@@ -10,6 +10,28 @@
 #~~ 1. Setup ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 #~~~~ 1a. Class creation and constructor functions
+setClass("foragerA", slots = list(location = "numeric", memory = "data.frame", optForageThresh = "numeric")) #simplified
+
+ForagerA<- function(location = c(runif(1,0, 10),runif(1,0, 10)),   
+                    memory = data.frame(patchName = "grass", xLocation = runif(1,0,10), yLocation = runif(1,0,10), 
+                                        resourceValAtDeparture = 3, timeSinceLastVisit  = 46, expectedValue = 87
+                    ), 
+                    optForageThresh = 0 #I changed this to 0 (a naive forager should eat any food available, not knowing if it will find any more)
+                    #instead of set values (0,10) can you have this take input xlim and ylim, then draw from -xlim to xlim (and same for y)
+                    #instead of assigning values to this dataframe, please make it an empty structure
+                    
+) 
+{
+  if(!length(location) == 2) stop ("location must be length 2") #good checks, thanks
+  if(!ncol(memory) == 6) stop ("memory should be a data frame with 6 columns")
+  newForager <- new("Forager", location = location, memory = memory, optForageThresh = optForageThresh)
+  names(newForager@location) <- c('x', 'y')
+  names(newForager@memory$xLocation) = 'x'
+  names(newForager@memory$yLocation) = 'y'
+  newForager
+}
+
+
 setClass("patchA", slots = list(location = 'numeric', energy = 'numeric'))  #sets class structure for patch type A
 
 PatchA <- function(xlim, ylim) {                                  #constructor for patchA class
