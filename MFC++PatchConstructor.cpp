cf9db808c9031dcdeee7019429640f1573b0496c
Cyrus Parvereshi <cjparvereshi@ucdavis.edu>
2018-01-25 08:08:29 -0800

    Basic skeleton of Patch class definition
    Here I just wanted to show the basic structure of the Patch class, and how the methods and data types are organized in a typical C++ class. I have a constructor (allocates memory for an object of the class) and a deconstructor (deallocates memory for an object of the class) set up bare-bones (you actually don't need to type anything in them to run; we will customize the constructor soon though). The next step is writing in the Constructor function and creating "getter" and "setter" methods. One thing I'd like to know though is what exactly a "Patch" consists of and what it does, so I have an absolutely clear picture of how to approach the class definition. Thanks!

--- MFC++PatchConstructor.cpp
+++ MFC++PatchConstructor.cpp
@@ -1,3 +1,54 @@
 //HEYYYYYY
+/* We up in here doing C++ shit now, translating Alex's and Sam's R code into C++ to do the hard work and be fast af
+We starting with the Patch class/constructor.
+LETS GET POPPIN'!
+*/
 #include <iostream>
-#include <cstdlib>
+using namespace std;
+
+
+//Patch class definition, 
+class Patch {
+    public: 
+    	//constructor
+        Patch(){
+
+        } 
+        virtual ~Patch(); //deconstructor
+        
+    private:
+       int xLim,yLim; // the data types, we use ENSCAPULATION, or hiding important info from the user to make this 
+       		          // data only accessible to us, the developers. 
+}; // end a class definition with a semicolon
+  
+
+int main(){return 0;} // this is just here to show that all C++ programs need a main function; I'll probably remove it later though
+
+                      // in good practice, we make a separate file for the class definition and a header file for the 
+                      // class prototype (which we would call "PatchConstructor.cpp")
+
+
+
+
+
+
+
+
+
+
+
+/*PatchA <- function(xlim, ylim) {                                  #constructor for patchA class
+  ###input: "xlim" = maximum value for x coordinate of patch
+  ###       "ylim" = maximum value for y coordinate of patch
+  ###output: object of class patchA
+  ###description: The location slot of output object will have x,y coordinates drawn from a uniform distribution between -xlim and xlim, and -ylim and ylim
+  ###             The energy slot of output will have value of 5
+  if(xlim <0 | ylim<0)
+  {
+    stop("must enter a positive value for xlim and ylim")
+  }
+  newPatchA <- new("patchA", location = c(runif(1, -xlim, xlim), runif(1, -ylim, ylim)), 
+                   energy = 5)
+  names(newPatchA@location) <- c('x', 'y')
+  newPatchA
+}*/
