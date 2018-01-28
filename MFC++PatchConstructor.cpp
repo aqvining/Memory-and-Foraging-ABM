1fa0cc960ad47fc138985a7e9e7062e02ef27418
Cyrus Parvereshi <cjparvereshi@ucdavis.edu>
2018-01-28 00:08:47 -0800

    numb gen redo, more constructor work
    I redid the random number generator function to borrow more stuff from C and modified the getters and setters and constructor more.

--- MFC++PatchConstructor.cpp
+++ MFC++PatchConstructor.cpp
@@ -1,14 +1,12 @@
-//HEYYYYYY
-/* We up in here doing C++ shit now, translating Alex's and Sam's R code into C++ to do the hard work and be fast af
-We starting with the Patch class/constructor.
-LETS GET POPPIN'!
-*/
+
 #include <assert.h> // to use assert function
 #include <iostream> // use basic facilities like cout and cin
-#include <random> // to generate random 
+#include <cstdlib> // for srand() and rand()
+#include <ctime> // for using time() to gen truly random numbers
+
 using namespace std;
 
-double genRandNum(int a, int b); // function prototype
+double genRandUnifNum(double a, double b); // function prototype
 
 //Patch class definition, 
 class Patch {
@@ -18,71 +16,67 @@ class Patch {
 	         // which will be explained in the public section
     	double location[2]; // location is coordinate pair, so should be array 
     	int energy; 
-    public: 
-    	//constructor
-        Patch(double xLim = 0 , double yLim =10, int Energy= 5){
-            if(xLim < 0.0 || yLim < 0.0){
-               assert("must enter a positive value for xlim and ylim"); // just like "stop" in R, but need to change the syntax lolw
-            }     
-             setLocation(xLim, yLim);
-            
-             setEnergy(Energy);
-             cout << 
+      public: 
+    	//constructor -> should define outside of function as Patch::Patch() and put prototype here. 
+        Patch(double xLim = 0.0 , double yLim =10.0, int Energy= 5){
+            assert(xLim < 0.0 || yLim < 0.0); // do checks real quick -> if want to be exactly as R code just need if() statement with cout
+            location[0] = genRandUnifNum(-1 * xLim, xLim); //use subscripts to assign each location to the array
+            location[1] = genRandUnifNum(-1 * yLim,ylim); //idk about how you "name" an array (what the equivalent of what we think is an R vector) in c++, you just
+            setEnergy(Energy);
+            cout << "You just created a patch with location: ("<< location[0]<<","<<location[1]<<") and with energy "<< getEnergy();
+            cout<< "\n";
         }
         //getters and setters
-        void setLocation(double xLim, double yLim){
-           location[0] = genRandNum(-1 * xLim, xLim); //use subscripts to assign each location to the array
-           location[1] = genRandNum(-1 * yLim,ylim); //idk about how you "name" an array (what the equivalent of what we think is an R vector) in c++, you just
+
+        //setter methods: let u set the values of the data members
+        void setLocation(double* Location){
+           
+           location = Location;
              // have to use subscripting. 
         }
         void setEnergy(int Energy){
         	energy = Energy;
         }
+        //getter methods: let you see the values of the data members and use them. 
         double* getLocation(){
-
+            return location;
         }
-       
-
-
+        int getEnergy(){
+         	return energy;
+        }
         virtual ~Patch(){} //deconstructor, I don't think we need to really elaborate this guy b/c it just deallocates
          	              // all memory allocated by the constructor. 	
-        
-  
-     
-}; // end a class definition with a semicolon
+};
+   // end a class definition with a semicolon
   
 
 int main(){
-   // lets start here and randomly generate xlim and ylim
-	int a,b; // I'm letting you enter integers for convenience, but if you cool with doubles, let me know and I'll change this. 
-    cout<<"Enter a and b for the uniform distribution!\n";
-    cout<<"Do \"a\" first: ";
-    cin>>a;
-    cout<<"Now input \"b\": ";
-    cin>>b;
-    /*I used code on this website: http://en.cppreference.com/w/cpp/numeric/random/uniform_real_distribution
-    *to help me create the randomly generated uniform distribution
-    */
-   
-    Patch patchA(); // instantiate a Patch using constructor -> can pass energy, xlim and ylim as arguments. 
-
-
+    Patch patchA(0.0,10); // instantiate a Patch using constructor -> can pass energy, xlim and ylim as arguments. 
     return 0;
 }  //I'll probably move this main function to another file soon and create header files and stuff
 
-double genRandNum(int a = 0, int b = 10){
-	random_device rd;  //Will be used to obtain a seed for the random number engine -> CYRUS NOTE: this is actually PSEUDO-random, not random,
-    		           // I need to find a way to seed it with the current time so it is truly random. 
-    mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd() -> CYRUS NOTE: 
-    uniform_real_distribution<> dis((double)a, (double)b); // create uniform random number generator between a and b
-                                                           // notice that I typecast a and b as doubles because the uniform real distribution
-                                                           // function only accepts doubles as arguments and outputs doubles too. 
-    xlim = dis(gen); // idk what gen or dis does lmao
-    ylim = dis(gen);
+//this function tries to randomly generate numbers on the uniform distribution but I think it doesn't work
+double genRandUnifNum(double a = 0.0, double b = 10.0){
+	// random_device rd;  //Will be used to obtain a seed for the random number engine -> CYRUS NOTE: this is actually PSEUDO-random, not random,
+ //    		           // I need to find a way to seed it with the current time so it is truly random. 
+ //    mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd() -> CYRUS NOTE: 
+ //    uniform_real_distribution<> dis((double)a, (double)b); // create uniform random number generator between a and b
+ //                                                           // notice that I typecast a and b as doubles because the uniform real distribution
+ //                                                           // function only accepts doubles as arguments and outputs doubles too. 
+ //    xlim = dis(gen); // idk what gen or dis does lmao
+ //    ylim = dis(gen);
+
+
+
+	// lol I chose to abandon the above code for basic C functions srand() and rand() to generate random (I think uniform) numbers 
+	   srand(time(0)); // initialize random seed, this might need to belong in the main function or global IDK
+	   //normally we wouldn't do so much with this next line, but we trying to randomly generate doubles, not ints. 
+       double randNum = a + static_cast <double> (rand()) / (static_cast <double> (RAND_MAX/(a-b)));
+       return randNum;
 }
 
                       // in good practice, we make a separate file for the class definition and a header file for the 
-                      // class prototype (which we would call "PatchConstructor.cpp")
+                      // class prototype (which we would call "PatchConstructor.h")
 
 
 
