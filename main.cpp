3cab9214efcc785a9b245981fab65a34ee74acbe
Cyrus Parvereshi <cjparvereshi@ucdavis.edu>
2018-02-14 16:24:52 -0800

    main, patch grid/matrix
    Added thoughts about Patch grid/matrix, and rewrote main function slightly

--- main.cpp
+++ main.cpp
@@ -6,28 +6,61 @@
 //#include "Forager.h"	
 using namespace std;
 
+/* Alex's desired thoughts and notes for 2/14/18
+
+numRows x numCols Patch matrix -> you need to define some kind of double patch pointer
+
+Patch** PatchMatrix = new [numRows] Patch*; // allocate space for rows of matrix
+for(int i = 0; i < numRows; i++){
+   Patch*[i] = new[numCols] Patch; // allocate space for columns
+}
+
+The issue still remains though how do you assign every patch a name or whatever?
+If you create a matrix, you will have to use index notation where PatchMatrix[0][0] is the top left corner Patch and 
+PatchMatrix[numRows][numCols] is bottom right corner Patch. I guess maybe that's ok since you could just say 
+PatchMatrix[0][0] is Patch 0 or A or 1 or whatever and PatchMatrix[0][1] is B or something. 
+
+I'm not sure what you are confused about for the multiple data values thing, but you can just add data members to the 
+Patch class and each Patch in the matrix will contain such values. 
+
+make sure that later in code deallocate memory:
+for(int j = 0; j < numCols;j++){ //delete columns
+   delete[] PatchMatrix*[j];
+}
+delete [] PatchMatrix; //delete rows
+ OR
+sticking with current method with 1 patch array(HELL NO)
+OR 
+alternative methods for reducing complexity fo checking location of every patch that exists
+diff info 
+-> just putting it out there: linked lists and vectors but those will take time to learn, implement, and have bigger processing time
+*/
 double genRandUnifNum(double a, double b); // function prototype
 void truncateCoords(Patch patch);
-int main(int argc, char* argv[]){ // consider having command line arguments -> argc, argv*[]
-      Patch* patchArray = new[argc-1] Patch; //create the number of patches typed on the command line 
-      for(int i = 1; i < argc; i++){ //skip first argument b/c it's the executable name
-         patchArray[i] = Patch(argv*[i]); // i doubt this works but the idea is to create in this array of patches a patch object with the name entered on the command line
-      }
+int main(int argc, char* argv[]){ // consider having command line arguments = exec name, number of patches, square sides (sets xlim and ylim)
+      const int NUMPATCH = static_cast<int>arg*v[1];
+      //we will have (0,0) as the center lol
+      const int XLIM = static_cast<int>argv*[2];  
+      const int YLIM = static_cast<int> argv*[3];
+       
+      Patch* patchArray = new[NUMPATCH] Patch; //allocate the number of patches typed on the command line into patchArray
+      // for(int i = 1; i < argc; i++){ //skip first argument b/c it's the executable name
+      //    patchArray[i] = Patch(argv*[i]); // i doubt this works but the idea is to create in this array of patches a patch object with the name entered on the command line
+      // } // modify
 // map containing all patches mapped to a key for patch number
-      map PatchList<string, Patch> x = {{"1", Patch Patch("PatchA")}, {"2", Patch Patch("PatchB")}, 
+    map PatchList<string, Patch> x = {{"1", Patch Patch("PatchA")}, {"2", Patch Patch("PatchB")}, 
                                    {"3", Patch Patch("PatchC")}, {"4", Patch Patch("Patch D")}};
     cout<<"Successfully randomly generated patchs\n";
     for(int patchNum = 0; patchNum < patchArray.size(); patchNum++){
        truncateCoords(patchArray[patchNum]);  
     }
-    
+    //this is a shit way of assigning patch names
     for(int j = 0; j < patchArray.size(); j++){
-       if(patchArray[j].location[0] > 0 && patchArray[j].location[0] <= 10){
-          if(patchArray[j].location[1] > 0 && patchArray[j].location[1] <=10 )
+       if(patchArray[j].location[0] > 0 && patchArray[j].location[0] <= 1){
+          if(patchArray[j].location[1] > 0 && patchArray[j].location[1] <=1 )
           Patch['1'] = patchArray[j]; // if patch btw (0,0) and (0,10) then put in patch 1
        } //etc 
-       else if()
-         
+       else if()         
     }
    
 
